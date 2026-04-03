155910a2ce07a85d9c5cfa0ab9cd45efd22fcf72
Aleksandar Krsteski <krsteski_aleksandar@hotmail.com>
2026-04-03 10:56:16 +0200

    Refactor: migrate MediaWiki connector to use MediaWikiReader, add auth support and env var config

--- tasks/base.py
+++ tasks/base.py
@@ -279,9 +279,6 @@ class IngestionJob(ABC):
 
         try:
             for item in self.list_items():
-                if self.request_delay > 0:
-                    time.sleep(self.request_delay)
-
                 count = self.process_item(item)
                 if count == 0:
                     skipped += 1
